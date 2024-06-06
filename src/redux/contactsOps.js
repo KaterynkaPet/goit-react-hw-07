74660199e15919ff2e837ef550c30e1a36e44f43
KaterynkaPet <116788145+KaterynkaPet@users.noreply.github.com>
2024-06-07 00:15:14 +0300

    created contactsOps

---
+++ src/redux/contactsOps.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const API_URL = 'https://66621f7063e6a0189feccbd4.mockapi.io/';
+
+export const fetchContacts = createAsyncThunk(
+    'contacts/fetchAll',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get(`${API_URL}/contacts`);
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.response.data);
+        }
+    }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, thunkAPI) => {
+    try {
+      const response = await axios.post(`${API_URL}/contacts`, contact);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      await axios.delete(`${API_URL}/contacts/${contactId}`);
+      return contactId;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);

396726563998f21d1078a151460bd22f4cce540f
KaterynkaPet <116788145+KaterynkaPet@users.noreply.github.com>
2024-06-07 00:02:07 +0300

    Added Status Form

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -12,12 +12,9 @@ const formatPhoneNumber = (value) => {
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    items: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    items: [],
+    loading: false,
+    error: null,
   },
   reducers: {
     addContact: {
@@ -38,9 +35,19 @@ const contactsSlice = createSlice({
     deleteContact(state, action) {
       state.items = state.items.filter(contact => contact.id !== action.payload);
     },
+
+    setLoading(state, action) {
+      state.loading = action.payload;
+    },
+
+    setError(state, action) {
+      state.error = action.payload;
+    },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
+export const { addContact, deleteContact, setLoading, setError } = contactsSlice.actions;
 export const selectContacts = (state) => state.contacts.items;
+export const selectLoading = (state) => state.contacts.loading;
+export const selectError = (state) => state.contacts.error;
 export default contactsSlice.reducer;
